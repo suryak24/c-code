bd8832d0081a81def69f6acda329c59d59931e26
suryak24 <36228729+suryak24@users.noreply.github.com>
2018-02-07 16:45:32 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<cpnio.h>
+void main()
+{
+char m;
+clrscr();
+if((m>=a && m<=z) || (m>=A && m<=Z))
+{
+printf("the given charecter is alphabet");
+}
+else
+{
+prinrf("the given charecter is not a alphabet");
+}
+getch();
+}
