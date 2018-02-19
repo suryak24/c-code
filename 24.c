001e3e1125c26ddbd3e3d7212fbeed3d63cbf0b9
suryak24 <36228729+suryak24@users.noreply.github.com>
2018-02-19 16:28:38 +0530

    Update 24.c

--- 24.c
+++ 24.c
@@ -6,20 +6,23 @@ int a[10],i,j,n,temp;
 clrscr();
 printf("Enter the n values:");
 scanf("%d",&n);
-for(j=0;j<n;j++)
+for(i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
 }
 for(j=0;j<n;j++)
 {
-if(a[i]<a[i+1])
+for(i=0;i<n;i++)
+{
+if(a[i]>a[i+1])
 {
 temp=a[i];
 a[i]=a[i+1];
 a[i+1]=temp;
 }
 }
-for(j=0;j<n;j++)
+}
+for(i=0;i<n;i++)
 {
 printf("%d",a[i]);
 }
