d78b03283403ca88b4d7185044753412c070de1c
suryak24 <36228729+suryak24@users.noreply.github.com>
2018-02-19 16:33:31 +0530

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],i,j,n,temp;
+clrscr();
+printf("Enter the n values:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(j=0;j<n;j++)
+{
+for(i=0;i<n;i++)
+{
+if(a[i]>a[i+1])
+{
+temp=a[i];
+a[i]=a[i+1];
+a[i+1]=temp;
+}
+}
+}
+printf("%d",a[i/2]);
+getch();
+}
+
