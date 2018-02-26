668f94aaaeac98211d1c4f3bdb9104df11748f7a
suryak24 <36228729+suryak24@users.noreply.github.com>
2018-02-26 16:22:23 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int i;
+  char s[100];
+  printf("Enter the string:");
+  scanf("%s",s[i]);
+   if(s[i]>=0 && s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='Z')
+     printf("yes");
+  return 0;
+}
+
+  
