7e9d45e668861d4a226cb5468393d7d6b3a1f3ee
suryak24 <36228729+suryak24@users.noreply.github.com>
2018-03-05 16:45:06 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,flag=0,num;
+clrscr();
+printf("Enter the number:");
+scanf("%d",&num);
+for(i=2;i<=num/2;i++)
+{
+ if(num%i==0)
+ {
+ flag=1;
+ break;
+ }
+}
+if(flag==0)
+{
+printf("no");
+}
+else
+{
+printf("yes");
+}
+getch();
+}
